6dcfa25c18969947c9580a8a4cf681a9f4a7c822
Gaston <gastno@fanbridge.com>
2014-02-20 04:39:35 -0300

    Themes clubber - latest tracks

---
+++ app/webroot/js/angular/app/controllers/themes/clubber/latest/tracks.js
@@ -0,0 +1,31 @@
+function latestTracksCo($scope,appSv,contentSv,$sce) {
+
+	$scope.contentSv= contentSv;
+
+	$scope.setList = function() {
+
+	}
+
+	$scope.$watch("contentSv.getPageList('tracks')",function(list){
+
+		if (angular.isDefined(list.list)) {
+			$scope.list = list.list.slice(0,$scope.limit);
+			//$scope.list = list.list;
+			$scope.setList();
+		}
+
+	},true);	
+
+	$scope.getDescription = function(index) {
+
+		return $sce.trustAsHtml(contentSv.getDescription($scope.list[index]));
+	}
+
+	$scope.getTitleStyle = function() {
+		return {
+			'background-color': $scope.config.custom.colors.contentBackground.value,
+			'color': $scope.config.custom.colors.contentText.value
+		}
+	}
+
+}
