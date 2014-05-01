f871add31f1b74c35181774c91085915cef0050a
Gaston <gastno@fanbridge.com>
2014-05-01 03:11:10 -0300

    Themes clubber - iframe responsive

--- app/webroot/js/angular/app/directives/themes/clubber/index.js
+++ app/webroot/js/angular/app/directives/themes/clubber/index.js
@@ -11,10 +11,17 @@ function(){
 
             var raw = $('body');
             scope.showUp = false;
+            scope.scrolling = false;
 
             scope.scrollToTop = function() {
+                if (scope.scrolling) {
+                    return false;
+                }
+                scope.scrolling = true;
                 scope.showUp = false;
                 $('body').animate({scrollTop: $('body').offset().top}, "slow");
+                scope.scrolling = false;
+
             }
 
             $(window).scroll(function() {
