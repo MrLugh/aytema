ef9bdc094e09aa75964235f92fcc04595a985d6c
Gaston <gastno@fanbridge.com>
2014-05-02 12:43:28 -0300

    Themes clubber - background image

--- app/webroot/js/angular/app/controllers/admin/background.js
+++ app/webroot/js/angular/app/controllers/admin/background.js
@@ -4,6 +4,8 @@ function adminBackgroundCo($scope,userSv,appSv,contentSv) {
 	$scope.user 	= userSv.getUser();
 	$scope.config	= $scope.$parent.config;
 
+	$scope.newBg	= {name:'',img:''};
+
 	$scope.saveConfig = function() {
 		userSv.saveThemeConfig();
 	}
@@ -11,5 +13,21 @@ function adminBackgroundCo($scope,userSv,appSv,contentSv) {
 	$scope.restoreConfig = function() {
 		userSv.restoreConfig();
 	}
+
+	$scope.canAdd = function() {
+
+		if ($scope.newBg.img.length>0 && $scope.newBg.name.length>0) {
+			return true;
+		}
+		return false;
+	}
+
+	$scope.addBackground = function() {
+		var bg = {};
+		bg[$scope.newBg.name] = $scope.newBg.img;
+		if (!angular.isDefined($scope.config.custom.background.list[bg])) {
+			$scope.config.custom.background.list[$scope.newBg.name] = $scope.newBg.img;
+		}
+	}	
 
 }
