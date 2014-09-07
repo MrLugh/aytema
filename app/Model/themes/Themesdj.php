262152003d6fc95df1d208c4c207f8a4b3a1ab9c
Gaston <gastno@fanbridge.com>
2014-09-07 02:00:35 -0300

    Admin - structure

---
+++ app/Model/themes/Themesdj.php
@@ -0,0 +1,42 @@
+<?php
+
+Class Themedj {
+
+    static $config = array(
+        'colors' => array(
+            'background' => array(
+                'value' => '#A69E9E',
+                'label' => 'Body background',
+            ),
+            'contentBackground' => array(
+                'value' => '#3B3232',
+                'label' => 'Content background',
+            ),
+            'contentText' => array(
+                'value' => '#F7F1E6',
+                'label' => 'Content text',
+            ),
+        ),
+        'fonts' => array(
+            'selected' => array(
+                'family'=> "'Ubuntu', sans-serif",
+                'size'  => '13px',
+            ),
+            'list' => array(
+                'Playfair Display' => "'Playfair Display', serif",
+                'Open Sans' => "'Open Sans', sans-serif",
+                'Josefin Sans' => "'Josefin Sans', sans-serif",
+                'Asap' => "'Asap', sans-serif",
+                'Ubuntu' => "'Ubuntu', sans-serif",
+                'Raleway' => "'Raleway', sans-serif",
+                'Lora' => "'Lora', serif",
+                'Montserrat' => "'Montserrat', sans-serif",
+                'Arvo' => "'Arvo', serif",
+                'Junge' => "'Junge', serif"
+            ),
+        ),
+        'width' => '100%',
+    );
+
+
+}
